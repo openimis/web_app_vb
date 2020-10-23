fb6120632d08223300b9df45d27d299745e82540
Dragos Dobre <dragos.dobre@swisstph.ch>
2020-10-23 15:16:46 +0200

    Update app version

--- IMIS_BI/My Project/AssemblyInfo.vb
+++ IMIS_BI/My Project/AssemblyInfo.vb
@@ -58,5 +58,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2020.10.19.9")>
+<Assembly: AssemblyVersion("2020.10.23.10")>
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
