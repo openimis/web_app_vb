b6f3c4f9151dae5fb87f65baedea9819a31e80c2
Dragos Dobre <dragos.dobre@swisstph.ch>
2020-10-26 09:54:11 +0100

    Update languages

--- IMIS_EN/My Project/AssemblyInfo.vb
+++ IMIS_EN/My Project/AssemblyInfo.vb
@@ -58,5 +58,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2020.10.23.14")>
+<Assembly: AssemblyVersion("2020.10.23.15")>
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
